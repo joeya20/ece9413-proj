003497e8761dd33628d5d938245fd2ad25476953
Joey Ah-kiow <joeyah20@gmail.com>
2024-03-06 13:12:18 -0500

    added script output

--- tests/FCL/gen.py
+++ tests/FCL/gen.py
@@ -1,12 +1,15 @@
-import random
+from random import randint
 
-with open('SDMEM.txt', '+w') as sdmem:
-    for i in range(256):
-        sdmem.write(f'{i}\n')
-    sdmem.write('66048')
+def main():
+    with open('SDMEM.txt', '+w') as sdmem:
+        for i in range(256):
+            sdmem.write(f'{i}\n')
+        sdmem.write('66048')
 
-with open('VDMEM.txt', '+w') as vdmem:
-    # write a, then b, then W
-    for _ in range(66048):
-        vdmem.write(f'{random.randint(-5, 5)}\n')
+    with open('VDMEM.txt', '+w') as vdmem:
+        # write a, then b, then W
+        for _ in range(66048):
+            vdmem.write(f'{randint(-5, 5)}\n')
     
+if __name__=='__main__':
+    main()
--- tests/FCL/validate.py
+++ tests/FCL/validate.py
@@ -1,15 +1,21 @@
 import numpy as np
 
-with open('VDMEMOP.txt') as vdmem:
-    vdmem_lines = vdmem.readlines()
+def main():
+    with open('VDMEMOP.txt') as vdmem:
+        vdmem_lines = vdmem.readlines()
+        
+    # we have to read directly from VDMEM for inputs since they are
+    # randomly generated using gen.py
+    a = np.array(vdmem_lines[:256], dtype=np.int32)
+    b = np.array(vdmem_lines[256:512], dtype=np.int32)
+    W = np.reshape(np.array(vdmem_lines[512:512+256*256], dtype=np.int32), (-1, 256))
 
-a = np.array(vdmem_lines[:256], dtype=np.int32)
-b = np.array(vdmem_lines[256:512], dtype=np.int32)
-W = np.reshape(np.array(vdmem_lines[512:512+256*256], dtype=np.int32), (-1, 256))
+    dot_res = np.dot(W, a)
+    dot_out = np.array(vdmem_lines[66048:66048+256], dtype=np.int32)
+    print(dot_res == dot_out)
+    final_res = dot_res + b
+    final_out = np.array(vdmem_lines[66304:66560], dtype=np.int32)
+    print(final_res == final_out)
 
-dot_res = np.dot(W, a)
-dot_out = np.array(vdmem_lines[66048:66048+256], dtype=np.int32)
-print(dot_res == dot_out)
-final_res = dot_res + b
-final_out = np.array(vdmem_lines[66304:66560], dtype=np.int32)
-print(final_res == final_out)
+if __name__=='__main__':
+    main()
